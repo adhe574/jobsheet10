74601dae1035aaac84a779bab19109a597630973
adhe574 <adhewidya378@gmail.com>
2024-11-08 11:00:25 +0700

    simpan

---
+++ Numbers01.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class Numbers01 {
+    public static void main(String[] args) {
+        Scanner sc01 = new Scanner(System.in);
+
+        int[][] myNumbers = new int[3][];
+        myNumbers[0] = new int[5];
+        myNumbers[0] = new int[3];
+        myNumbers[0] = new int[1];
+
+        for (int i=0; i<myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for (int i=0; i<myNumbers.length; i++) {
+            System.out.println("Panjang baris ke-" + (i+1) + " : " + myNumbers[i].length);
+        }
+    }    
+}
